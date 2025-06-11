9e3b0926a5283775c4d3b9749c2c08c8e803d8b7
Pujasmn <vaktisa2000@gmail.com>
2025-06-11 10:18:54 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,7 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     outDir: 'dist',
-//     emptyOutDir: true,
-//     rollupOptions: {
-//       output: {
-//         manualChunks: {
-//           vendor: ['react', 'react-dom', 'react-router-dom'],
-//         },
-//       },
-//     },
-//   },
-//   server: {
-//     port: 3000,
-//   },
-// });
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // 👈 important for Netlify deployment
   plugins: [react()],
   build: {
     outDir: 'dist',
