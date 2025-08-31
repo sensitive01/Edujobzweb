1269c27959652672f8557eaa46eba01d5c84e5e7
Aswinrajr <aswinrajr07@gmail.com>
2025-08-31 11:17:50 +0530

    casesenstive checked

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AboutPage from './pages/public/About';
 import CareersPage from './pages/public/Careers';
 import BlogsPage from './pages/public/Blogs';
 import BlogDetailsPage from './pages/public/Blogs/[id]';
-import JobsPage from './pages/public/Jobs/Index';
+import JobsPage from './pages/public/Jobs/JobsPage';
 import JobsPageList from './pages/public/Jobs/JobsPageList';
 import JobPageMapGrid from './pages/public/Jobs/JobPageMapGrid';
 import JobsFilter from './pages/public/Jobs/JobsFilter';
